1dce833401b6447f0b6c95cd814e530ece67b3e7
juxtapos <claus@formatvorlage.de>
2011-07-23 18:03:22 +0200

    added testcase for resources

--- test/test-resources.js
+++ test/test-resources.js
@@ -1,19 +1,42 @@
-var nodeunit = require('nodeunit')
-	, resources = require('../lib/resources.js')
-	, promises  = require('promised-io')
-
-var testfiles = [
-	"../modules/app/application.css", "../modules/app/nothere.js", "./resources.js"
-];
+var nodeunit	 		= require('nodeunit')
+	, mod_resources 	= require('../lib/resources.js')
+	, promises  		= require('promised-io')
+	, assert			= require('assert')
 
 module.exports = nodeunit.testCase({
 	setUp : function (callback) {
-		resources.configure({"server":{"serverRoot" : __dirname + 'x'}});
 		callback();
 	}
 
-	, testLoadUrls : function (test) {
-		resources.loadUrls(testfiles).then(function (data) {
+	, testFileSimple : function (test) {
+		var resource1 = new mod_resources.FileResource();
+		assert.notEqual(resource1, null);
+		assert.throws(function () { resource1.load(); });
+		assert.equal(resource1.state, mod_resources.Resource.STATE.INIT);
+		// [TBD] change path
+		assert.throws(function () { resource1.load('/Users/cag/workspace/rain/server.js') });
+		assert.doesNotThrow(function () { resource1.load('file:///Users/cag/workspace/rain/server.js') });
+		assert.equal(resource1.state, mod_resources.Resource.STATE.LOADING);
+		resource1.addListener('stateChanged', function (res) {
+			assert.equal(res.state, mod_resources.Resource.STATE.READY);
+			test.done();
+		});
+	}
+
+	, testHttpSimple : function (test) {
+		var resource2 = new mod_resources.HttpResource();
+		assert.notEqual(resource2, null);
+		assert.throws(function () { resource2.load();});
+		assert.equal(resource2.state, mod_resources.Resource.STATE.INIT);
+		assert.throws(function () { resource2.load('heise.de') });
+		assert.doesNotThrow(function () { resource2.load('http://heise.de') });
+		assert.throws(function () { resource2.load(); });
+		// [TBD] how to do error handling in case in domain can't be resolved? 
+		// doesn't seem to be possible when using http.get 
+		//assert.throws(function () { resource2.load('http://127.255.255.1/I/dont/exist') });
+		assert.equal(resource2.state, mod_resources.Resource.STATE.LOADING);
+		resource2.addListener('stateChanged', function (res) {
+			assert.equal(res.state, mod_resources.Resource.STATE.READY);
 			test.done();
 		});
 	}
