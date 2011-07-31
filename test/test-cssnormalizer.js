8cbb4b4489bc16935e59d42c963901cac219096e
juxtapos <claus@formatvorlage.de>
2011-08-01 00:08:15 +0200

    OBEY!

--- test/test-cssnormalizer.js
+++ test/test-cssnormalizer.js
@@ -7,7 +7,7 @@ module.exports = nodeunit.testCase({
 		var exp = 'p{background-image:url("/modules/app/test/me.css")}\n\ntd { \n background-image: url("/foo.png");\ncolor:red}\na {\n background-image: url("/modules/app/bar.png");\n\n@import url("/modules/app/grunz.css")';
 
 		var ncss = mod_cssnormalizer.normalize(css, '/modules/app');
-		test.equal(ncss, exp)
+		test.equal(ncss, exp);
 		test.done();
 	}
 
