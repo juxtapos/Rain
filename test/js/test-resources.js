5526e25904f47cb4e67c40ab4483f97166874fb4
juxtapos <claus@formatvorlage.de>
2011-08-30 02:31:05 +0200

    using dedicated testfiles, requires slowserver.js to run

--- test/js/test-resources.js
+++ test/js/test-resources.js
@@ -1,93 +1,94 @@
-var nodeunit	 		= require('nodeunit')
-	, mod_resources = require('resources.js')
-	, Resource			= mod_resources.Resource
-	, promises  		= require('promised-io/lib/promise')
-	, assert				= require('assert')
-	, mod_path			= require('path')
+var nodeunit            = require('nodeunit')
+    , mod_resources     = require('resources.js')
+    , Resource          = mod_resources.Resource
+    , promises          = require('promised-io/lib/promise')
+    , assert            = require('assert')
+    , mod_path          = require('path')
 
 module.exports = nodeunit.testCase({
-	setUp : function (callback) {
-		callback();
-	},
+    setUp : function (callback) {
+        callback();
+    },
 
-	simpleDependency : function (test) {
-		var url1 = 'file://' + mod_path.join(__dirname, '..', '/lib/server.js')
-			, url2 = 'file://' + mod_path.join(__dirname, '..', '/modules/app/htdocs/index.html')
+    simpleDependency : function (test) {
+        var url1 = 'file://' + mod_path.join(__dirname, './testfiles/server.js')
+            , url2 = 'http://127.0.0.1:1338/'
 
-		var r1 = new Resource(url1);
-		var r2 = new Resource(url2);
-		r1.addDependency(r2);
+        var r1 = new Resource(url1);
+        var r2 = new Resource(url2);
+        r1.addDependency(r2);
 
-		r1.load();
-		r1.loadDependencies();
+        r1.load();
+        r1.loadDependencies();
 
-		r1.addListener('stateChanged', function (resource) {
-			if (resource.state == Resource.STATES.COMPLETE) test.done();
+        r1.addListener('stateChanged', function (resource) {
+            if (resource.state == Resource.STATES.COMPLETE) test.done();
 
-		});
-	}, 
+        });
+    }, 
 
-	chainImplicit : function (test) {
-		var url1 = 'file://' + mod_path.join(__dirname, '..', '/lib/server.js')
-			, url2 = 'file://' + mod_path.join(__dirname, '..', '/modules/app/htdocs/index.html')
-			, url3 = 'file://' + mod_path.join(__dirname, '..', '/modules/app/htdocs/index.html')
+    chainImplicit : function (test) {
+        var url1 = 'file://' + mod_path.join(__dirname, './testfiles/server.js')
+            , url2 = 'http://127.0.0.1:1338/'
+            , url3 = 'file://' + mod_path.join(__dirname, './testfiles/index.html')
 
-		var r1 = new Resource(url1);
-		var r2 = new Resource(url2);
-		var r3 = new Resource(url3);
-		r1.addDependency(r2);
-		r2.addDependency(r3);
+        var r1 = new Resource(url1);
+        var r2 = new Resource(url2);
+        var r3 = new Resource(url3);
+        r1.addDependency(r2);
+        r2.addDependency(r3);
 
-		r1.load();
-		r2.load();
-		r3.load();
+        r1.load();
+        r2.load();
+        r3.load();
 
-		r2.state = Resource.STATES.LOADING;
+        r2.state = Resource.STATES.LOADING;
 
-		r1.addListener('stateChanged', function (resource) {
-			if (resource.state == Resource.STATES.COMPLETE) {
-				test.done();
-			}
-		});
-	},
+        r1.addListener('stateChanged', function (resource) {
+            if (resource.state == Resource.STATES.COMPLETE) {
+                test.done();
+            }
+        });
+    },
 
-	complexDependencies : function (test) {
-		var url1 = 'file://' + mod_path.join(__dirname, '..', '/lib/server.js')
-			, url2 = 'file://' + mod_path.join(__dirname, '..', '/modules/app/htdocs/index.html')
-			, url3 = 'file://' + mod_path.join(__dirname, '..', '/modules/app/htdocs/index.html')
-			, url4 = url1
+    complexDependencies : function (test) {
+        var url1 = 'file://' + mod_path.join(__dirname, './testfiles/server.js')
+            , url2 = 'file://' + mod_path.join(__dirname, './testfiles/index.html')
+            , url3 = 'file://' + mod_path.join(__dirname, './testfiles/index.html')
+            , url4 = 'http://127.0.0.1:1338/'
 
-		var r1 = new Resource(url1);
-		var r2 = new Resource(url2);
-		var r3 = new Resource(url3);
-		var r4 = new Resource(url4);
-		r3.addDependency(r4);
-		r1.addDependency(r2);
-		r1.addDependency(r3);
+        var r1 = new Resource(url1);
+        var r2 = new Resource(url2);
+        var r3 = new Resource(url3);
+        var r4 = new Resource(url4);
+        r3.addDependency(r4);
+        r1.addDependency(r2);
+        r1.addDependency(r3);
 
-		r1.load();
-		r1.loadDependencies();
-		r2.load();
-		r3.load();
-		r4.load();
+        r1.load();
+        r1.loadDependencies();
+        r2.load();
+        r3.load();
+        r4.load();
 
-		r1.addListener('stateChanged', function (resource) {
-			if (resource.state == Resource.STATES.COMPLETE) {
-				test.done();
-			}
-		});
-	},
+        r1.addListener('stateChanged', function (resource) {
+            console.log(resource.uuid + ': state ' + resource.state);
+            if (resource.state == Resource.STATES.COMPLETE) {
+                test.done();
+            }
+        });
+    },
 
-	addDependencyAfterLoadStarted : function (test) {
-		var url1 = 'file://' + mod_path.join(__dirname, '..', '/lib/server.js')
-			, url2 = 'file://' + mod_path.join(__dirname, '..', '/modules/app/htdocs/index.html');
-		var r1 = new Resource(url1);
-		var r2 = new Resource(url2);
-		r1.load();
-		r1.addDependency(r2);
+    addDependencyAfterLoadStarted : function (test) {
+        var url1 = 'file://' + mod_path.join(__dirname, './testfiles/server.js')
+            , url2 = 'file://' + mod_path.join(__dirname, './testfiles/index.html');
+        var r1 = new Resource(url1);
+        var r2 = new Resource(url2);
+        r1.load();
+        r1.addDependency(r2);
     r2.load();
     r1.addListener('load', function (resource) {
       test.done();
     });
-	}
+    }
 });
---
+++ test/js/testfiles/server.js
@@ -0,0 +1,151 @@
+/*
+Copyright (c) 2011, Claus Augusti <claus@formatvorlage.de>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+"use strict";
+
+/**
+ * Main server.
+ * Handles a very simple kind of dependency injection, to be refactored to some service
+ * registry ting. 
+ * 
+ */
+ 
+    var mod_connect         = require('connect')
+    , mod_sys               = require('sys')
+    , mod_path              = require('path')
+    , mod_resourceservice   = null
+    , mod_resourcemanager   = null
+    , mod_modulecontainer   = null
+    , mod_tagmanager        = require('./tagmanager.js')
+    , mod_cache             = require('./cache.js')
+    , mod_socketio          = require('./socketio.js')   
+    , mod_modules           = null
+    , mod_fs                = require('fs')
+    , cache                 = null
+    , redisclient           = null
+    , logger                = require('./logger.js').getLogger(mod_path.basename(module.filename))
+
+// [TBD] dynamic configServer service (requires dependency management first)
+
+/** TODO: remove if debugging information from --debug fixed with an endOfLine */    
+console.log('');
+    
+var configServer = null,
+    configModules = null,
+    args = {};
+
+for (var i = 2, l = process.argv.length; i < l; i++) {
+  var arg = process.argv[i];
+  if (arg.indexOf('=') > 1) {
+    arg = arg.split('=');
+    args[arg[0]] = arg[1];
+  }
+}
+
+if(!args['server-conf']) {
+    logger.info('set default: server.conf.default');
+    args['server-conf'] = mod_path.join(__dirname, '..', 'conf', 'server.conf.default');
+}
+logger.info('reading server configuration from ' + args['server-conf']);
+mod_fs.readFile(args['server-conf'], function (err, data) {
+    if (err) {
+        logger.error('error reading server configuration');
+        process.exit();
+    }
+    
+    configServer = JSON.parse(data);
+    logger.info('server configuration loaded');
+    configureServer(configServer);
+});
+
+// [TBD] move to module container
+// if(!args['module-conf']) {
+//   logger.info('set default: module.conf.default');
+//   args['module-conf'] = mod_path.join(__dirname, '..', 'conf', 'module.conf.default');
+// }
+// logger.info('reading webcomponent configuration from ' + args['module-conf']);
+// mod_fs.readFile(args['module-conf'], function (err, data) {
+//   if (err) {
+//       logger.error('error reading webcomponent configuration');
+//       process.exit();
+//   }
+  
+//   configModules = JSON.parse(data);
+//   logger.info('webcomponent configuration loaded');
+//   if(configServer)
+//     configureServer(configServer, configModules);
+//  
+// });
+
+function configureServer (configServer) {
+  
+  mod_cache.configure(configServer);
+  mod_resourcemanager = require('./resourcemanager.js')(configServer, mod_cache);
+  mod_modulecontainer = require('./modulecontainer.js')();
+  mod_resourceservice = require('./resourceservice.js')(configServer, mod_resourcemanager, mod_cache, mod_modulecontainer);
+  mod_tagmanager.setTagList(configServer.taglib);
+  mod_modules = require('./modules.js')(mod_tagmanager, mod_resourcemanager, mod_modulecontainer);
+
+  if (configServer.remotecontrol) {
+      logger.info('setting up remote control');
+      redisclient = require('./redisclient.js');
+      redisclient.init(mod_tagmanager);
+  }
+
+  createServer(configServer);
+  logger.info('server started on port ' + configServer.server.port + ', ' + new Date());
+}
+
+function createServer(configServer) {
+    var server = mod_connect.createServer(
+        mod_connect.favicon()
+        , mod_connect.bodyParser()
+        , mod_connect.router(function (app) {
+                app.get(/^\/modules\/([^\/]*)\/(.*\.html)$/,                mod_modules.handleViewRequest);            
+                app.get(/^\/modules\/([^\/]*)\/controller\/(.*\.js)$/,      mod_modules.handleControllerRequest);
+                app.put(/^\/modules\/([^\/]*)\/controller\/(.*\.js)$/,      mod_modules.handleControllerRequest);
+                app.post(/^\/modules\/([^\/]*)\/controller\/(.*\.js)$/,     mod_modules.handleControllerRequest);
+                app.delete(/^\/modules\/([^\.]*)\/controller\/(.*\.js)$/,   mod_modules.handleControllerRequest);
+                app.get(/^\/resources(.*)$/,                                mod_resourceservice.handleRequest);
+                //app.get(/instances\/(.*)/,                                  mod_instances.handleInstanceRequest);
+            }
+        )
+        , mod_connect.static(configServer.server.documentRoot)        
+        , mod_connect.logger()
+    );
+    
+    if (configServer.websockets) {
+        logger.info('starting websockets');
+        var io = require('socket.io').listen(server);
+        mod_socketio.init(io);
+    }
+    server.listen(configServer.server.port);
+}
+
+// process.on('SIGINT', function () {
+//  process.exit();
+// });
