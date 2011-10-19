6fdab9735d73d4351ea1d8250a10cde4e47332a7
mtschimev <privat@tschimev.de>
2011-10-19 13:52:56 +0200

    bugfix run_mothership.js script and added plubic directory for project creation

--- run_mothership.js
+++ run_mothership.js
@@ -1,1 +1,5 @@
-var mothership = require('./lib/mothership/server.js');
+var mothership = require('./lib/mothership/server.js')
+    ,fs = require('fs');
+
+var conf = JSON.parse(fs.readFileSync("./conf/mothership.conf.default").toString());
+mothership(conf);
