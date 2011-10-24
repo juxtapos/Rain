1e1725818575e8dd1b4fc3d5bf89e53ca990da1b
mtschimev <privat@tschimev.de>
2011-10-24 17:19:08 +0200

    removed hardcoded module and replaced all paths with path from configfile, done other stuff

--- modules/app/controller/js/index.html.js
+++
@@ -1,6 +0,0 @@
-
-function handleRequest (req, res) {
-	console.log('app index server side controller');
-}
-
-exports.handleRequest = handleRequest;
--- modules/weather/test/test.js
+++
@@ -1,4 +0,0 @@
-var content = require('fs').readFileSync('./testfile.txt').toString();
-var desc = content.match(/<!\[CDATA\[([\s\S]+)\]\]>/m)[0];
-
-console.log(desc);
