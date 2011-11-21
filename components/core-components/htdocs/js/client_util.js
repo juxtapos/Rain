a018f572bb89680a99edae9f19f774437c78ac95
Alexandru Bularca <alexandru.bularca@1and1.ro>
2011-11-21 18:01:08 +0200

    forgot to merge client_util

--- components/core-components/htdocs/js/client_util.js
+++ components/core-components/htdocs/js/client_util.js
@@ -3,23 +3,38 @@
  * @version 1.0
  */
 
-define(function(require) {
-	function ClientUtil() { }
-		
-	ClientUtil.prototype._create = function(proto) {
-		function F() { };
-		
-		F.prototype = proto;
-		
-		return new F();		
-	}
-	
-	ClientUtil.prototype.inherits = function(ctor, superCtor) {
-	  ctor.super_ = superCtor;
-	  ctor.prototype = this._create(superCtor.prototype);
-	}	   	
-	
-	var clientUtil = new ClientUtil();
-	
-	return {"inherits": clientUtil.inherits};
+define(function (require) {
+    function ClientUtil () {
+    }
+
+    ClientUtil.prototype._create = function (proto) {
+        function F () {
+        };
+
+        F.prototype = proto;
+
+        return new F();
+    }
+
+    ClientUtil.prototype.inherits = function (ctor, superCtor) {
+        ctor.super_ = superCtor;
+        ctor.prototype = this._create(superCtor.prototype);
+    }
+
+    ClientUtil.prototype.inject = function (destination, source) {
+        for (var key in source) {
+            if (source.hasOwnProperty(key)) {
+                destination[key] = source[key];
+            }
+        }
+
+        return destination;
+    }
+
+    /*var clientUtil = new ClientUtil();
+
+     return {"_create":clientUtil._create,
+     "inherits":clientUtil.inherits};*/
+
+    return new ClientUtil();
 });
