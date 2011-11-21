b0da436f913739b35ca86db62186c74b8701dc3c
rcosnita <radu.cosnita@gmail.com>
2011-11-21 09:12:31 +0200

    I improved the intents registry with a richer context that allows easy access to provider. Currently only view providers are supported for intents.

--- tests/server/util_loader.js
+++ tests/server/util_loader.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2011, Cosnita Radu Viorel <radu.cosnita@gmail.com>
+Copyright (c) 2011, Cosnita Radu Viorel <radu.cosnita@1and1.ro>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
